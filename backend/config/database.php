1b68872233cd102b24a01e4306da246740377f57
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-14 21:33:31 +0200

    chore:fix the database connection

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";   
 $user = "root";
 $pass = "";
 $db   = "ticket_system";
+$port = 3307;          
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Database connection failed");
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
